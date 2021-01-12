7a7aeaf6f206b5f0d1434b0d15528a3f0fa21ebc
Harshil <harshilsheth181888@gmail.com>
2021-01-12 21:10:07 +0530

    Initial Files Upload
    Covid Tracker Using ReactJs.

---
+++ build/precache-manifest.a2bc2c5fbbe539951395ec95ed538e98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75c306c615e365308b377fbe56b4d61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "48b8ddf49b13df865803",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/js/2.ad8e3933.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.ad8e3933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b8ddf49b13df865803",
+    "url": "/static/js/main.880ba2fb.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
